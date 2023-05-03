03b93ff66f3faa979740428ddf4f86713388eaa7
tlatmsrud <tlatmsrud@naver.com>
2023-05-03 17:45:44 +0900

    first commit

---
+++ jpa_study_space/src/main/java/org/study/jpa/Main.java
@@ -0,0 +1,57 @@
+package org.study.jpa;
+
+import org.study.jpa.space.objectmapping.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_study_space");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+            tx.begin();
+            logic(em);
+            tx.commit();
+
+        }catch(Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+    public static void logic(EntityManager em){
+
+        String id = "id1";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("테스터");
+        member.setAge(2);
+
+        em.persist(member);
+
+        member.setAge(20);
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember = " + findMember.getUsername() + ", "+ findMember.getAge());
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        System.out.println("size : "+ members.size());
+
+        em.remove(member);
+
+    }
+}
